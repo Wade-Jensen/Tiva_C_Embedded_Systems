aa6aba3df6048e01f450f93578e851f328057369
Wade Jensen <wadejensenwork@gmail.com>
2017-04-04 19:02:36 +1000

    uart example without ROM_ functions

---
+++ uart_echo/Debug/drivers/subdir_vars.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/frame.c \
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/kentec320x240x16_ssd2119.c \
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/pinout.c 
+
+C_DEPS += \
+./drivers/frame.d \
+./drivers/kentec320x240x16_ssd2119.d \
+./drivers/pinout.d 
+
+OBJS += \
+./drivers/frame.obj \
+./drivers/kentec320x240x16_ssd2119.obj \
+./drivers/pinout.obj 
+
+OBJS__QUOTED += \
+"drivers\frame.obj" \
+"drivers\kentec320x240x16_ssd2119.obj" \
+"drivers\pinout.obj" 
+
+C_DEPS__QUOTED += \
+"drivers\frame.d" \
+"drivers\kentec320x240x16_ssd2119.d" \
+"drivers\pinout.d" 
+
+C_SRCS__QUOTED += \
+"C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/frame.c" \
+"C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/kentec320x240x16_ssd2119.c" \
+"C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/pinout.c" 
+
+
---
+++ uart_echo/Debug/uart_echo.d
@@ -0,0 +1,37 @@
+# FIXED
+
+uart_echo.obj: ../uart_echo.c
+uart_echo.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdint.h
+uart_echo.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdbool.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/inc/hw_ints.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/inc/hw_memmap.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/inc/hw_types.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/debug.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/interrupt.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/uart.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/rom.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/rom_map.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/grlib/grlib.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/kentec320x240x16_ssd2119.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/frame.h
+uart_echo.obj: C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/pinout.h
+
+../uart_echo.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdbool.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/inc/hw_ints.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/inc/hw_memmap.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/inc/hw_types.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/debug.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/interrupt.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/uart.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/rom.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/driverlib/rom_map.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/grlib/grlib.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/kentec320x240x16_ssd2119.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/frame.h: 
+C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Repos/code_composer/TivaWare_C_Series-2.1.3.156/examples/boards/dk-tm4c129x/drivers/pinout.h: 
